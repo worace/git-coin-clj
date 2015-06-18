8eed5d71aa796cf3d40420331a7f5d16bfbf7d19
worace <horace.d.williams@gmail.com>
2015-06-18 15:28:30 -0600

    Experimenting with some async channel communication

--- src/git_coin_clj/core.clj
+++ src/git_coin_clj/core.clj
@@ -9,13 +9,16 @@
 (def server-url (or (System/getenv "COIN_SERVER") "http://localhost:9292"))
 (def target-url (str server-url "/target"))
 
+;; miners will send notifs to this channel when they find a coin
+(def coin-notifs (async/chan 10))
+
 (defn fetch-target [] (:body (http/get target-url)))
 
 (def current-target (atom 0))
 
 (defn hash-num [hash] (read-string (str "0x" hash)))
 
-(defn update-target []
+(defn update-target! []
   (swap! current-target (fn [_] (hash-num (fetch-target)))))
 
 (defn gen-hash [string] (digest/sha-1 string))
@@ -30,9 +33,20 @@
     (let [hash (gen-hash message)]
       (println (str "msg: " message))
       (println (str "hash: " hash))
+      (if (lower-hash? hash @current-target)
+        (do
+          (println (str "GOT A COIN: " hash))
+          (async/>!! coin-notifs {:hash hash :message message}) ))
       (if (< iterations 5)
         (recur hash (inc iterations)))
       )
     ))
 
+(async/go
+  (while true
+    (println (async/<! coin-notifs))))
+
+(update-target!)
+(println @current-target)
 (mine 1)
+
